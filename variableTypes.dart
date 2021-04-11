abf8487c298679e5d88360474f0a8004388b9bea
franMx <franMx@users.noreply.github.com>
2021-04-11 07:20:01 -0500

    Create variableTypes.dart

---
+++ variableTypes.dart
@@ -0,0 +1,25 @@
+//https://youtu.be/Ej_Pcr4uC2Q?t=865
+void main(){
+ /*
+  int
+  double
+  String
+  bool
+  dynamic
+  * */ 
+  
+  int amount1 = 1000;
+  var amount2 = 200;
+  print (amount1 + amount2);
+  
+  String name="Paco";
+  print ('my name is '+name);
+  print ('my name is $name'); // same as above
+  
+  double cambio = 5.50;
+  print("el cambio de las tortillas es $cambio");
+  
+  bool diabloEsBueno=false;
+  print('el diablo es bueno = $diabloEsBueno');
+
+}
