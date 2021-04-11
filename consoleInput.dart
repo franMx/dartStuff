03b07078416e379dc0b142e32171d86abe63eaf5
franMx <franMx@users.noreply.github.com>
2021-04-11 07:12:16 -0500

    Create consoleInput.dart

---
+++ consoleInput.dart
@@ -0,0 +1,16 @@
+import 'dart:io';//to be able to use input.
+
+void main() {
+  stdout.writeln('what is your name? ');
+  String name = stdin.readLineSync();
+  print('My name is $name');
+  for (int i = 0; i < 5; i++) {
+    dameNumero(i);
+  }
+}
+
+void dameNumero(int i){
+  String name="Paco";
+  print('hello ${i + 1} '+name);
+    //print('hello +${i}');
+}
